d2ffb75271d8d058d9f58316016666c9e873f324
bhutteomkar45 <bhutteomkar@gmail.com>
2026-02-04 16:54:13 +0530

    Add files via upload

---
+++ reverse array.cs
@@ -0,0 +1,17 @@
+﻿Build started at 16:39...
+1>------ Build started: Project: basicodes1, Configuration: Debug Any CPU ------
+1>  Skipping analyzers to speed up the build. You can execute 'Build' or 'Rebuild' command to run analyzers.
+1>D:\c#.net\basicodes1\traversestring.cs(7,18,7,32): warning CS8981: The type name 'traversestring' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\countintegers.cs(5,18,5,31): warning CS8981: The type name 'countintegers' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\adddofdigt.cs(7,18,7,28): warning CS8981: The type name 'adddofdigt' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\palindnum.cs(7,18,7,27): warning CS8981: The type name 'palindnum' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\reverseint.cs(7,18,7,28): warning CS8981: The type name 'reverseint' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\factorial.cs(7,18,7,27): warning CS8981: The type name 'factorial' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\array.cs(7,18,7,23): warning CS8981: The type name 'array' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\arithmaticop.cs(7,18,7,30): warning CS8981: The type name 'arithmaticop' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\fizzbuzz.cs(7,18,7,26): warning CS8981: The type name 'fizzbuzz' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\findmaxno.cs(7,18,7,27): warning CS8981: The type name 'findmaxno' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>D:\c#.net\basicodes1\guessnumber.cs(7,18,7,29): warning CS8981: The type name 'guessnumber' only contains lower-cased ascii characters. Such names may become reserved for the language.
+1>  basicodes1 -> D:\c#.net\basicodes1\bin\Debug\net10.0\basicodes1.dll
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 16:39 and took 03.289 seconds ==========
