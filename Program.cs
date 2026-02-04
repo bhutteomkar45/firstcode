bb3138822568d958474caad0015cd3be00729f3d
bhutteomkar45 <bhutteomkar@gmail.com>
2026-02-04 16:53:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace basicodes1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            guessnumber.findno();
+            arithmaticop.Mathprob();
+            fizzbuzz.fizzprog();
+            findmaxno.Maxin3();
+            factorial.factprod();
+            traversestring.travstr();
+            printtill10.print10();
+            eventill10.even1to10();
+            squareof10.prntsqr10();
+            countintegers.countnumbers();
+            adddofdigt.adddig2();
+            reverseint.reversenum
+            palindnum.palnumber();
+            array.revarray();
+        }
+    }
+}
+
