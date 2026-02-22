f90f5614b4e0553878b78786b1d0599b6aad7875
bhutteomkar45 <bhutteomkar@gmail.com>
2026-02-23 01:01:45 +0530

    Add files via upload

---
+++ Public,internal_mod/Privatemodifier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Public_mod
+{
+    public class Car
+    {
+
+        public required string model;
+        public required string Colour;
+        public int Price;
+
+
+
+        public void PrintCarInformation()
+        {
+            Console.WriteLine("Model:" + model);
+            Console.WriteLine("Colour:" + Colour);
+            Console.WriteLine("Price:" + Price);
+        }
+    }
+    internal class Car2
+    {
+        public required string model;
+        public required string Colour;
+        public int Price;
+
+
+        public void PrintCarInformation2()
+        {
+            Console.WriteLine("Model:" + model);
+            Console.WriteLine("Colour:" + Colour);
+            Console.WriteLine("Price:" + Price);
+        }
+    }
+}
---
+++ Public,internal_mod/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Public_mod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("# We are in private Access Modifier!\n");
+            Car myCar = new Car
+            {
+                model = "Farrari",
+                Colour = "Velwet",
+                Price = 30000000
+            };
+            myCar.PrintCarInformation();
+
+            Console.WriteLine("\n\n# We are in Internal Access Modifier!\n");
+            Car2 myCar2 = new Car2
+            {
+                model = "BMW",
+                Colour = "maroon",
+                Price = 2000000
+            };
+            myCar2.PrintCarInformation2();
+
+        }
+    }
+}
